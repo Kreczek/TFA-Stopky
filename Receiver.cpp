df328df95c465d88f50f2e69412d2f3417e0a1ac
Kreczek <Kreczek@users.noreply.github.com>
2019-05-22 23:45:39 +0200

    Update Receiver.cpp

--- Receiver.cpp
+++ Receiver.cpp
@@ -14,7 +14,6 @@ void setup() {
   /* and clear the display */
   lc.clearDisplay(0);
 
-  Serial.begin(9600);
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
