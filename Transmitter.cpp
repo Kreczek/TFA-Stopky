d8409f946ec08ea2ff9f55753b3f9e9d005009a7
rakat0 <52248245+rakat0@users.noreply.github.com>
2019-09-01 09:43:01 +0200

    Update Transmitter.cpp

--- Transmitter.cpp
+++ Transmitter.cpp
@@ -1,14 +1,18 @@
-// TFA STOPKY  vysilac
-// verze 0.8 30.6.2019
-//RF24 vysilac, puvodni pajsl
+//  TFA Stopky s nRF24L01
+//  verze 1.03
+//  2019.08.29
 
+
+
+
+#include <LiquidCrystal.h>
 #include <Arduino.h>
 #include <RF24.h>
 #include "elapsedMillis.h"
-#include <LiquidCrystal_PCF8574.h>
+
 
 RF24 radio(8, 9); // CE, CSN
-LiquidCrystal_PCF8574 lcd(0x27);
+
 elapsedMillis casomira;
 elapsedMillis displayRefreshTime;
 elapsedMillis stopRefresh;
@@ -16,7 +20,7 @@ elapsedMillis deBounce;
 
 const byte address[6] = "00001";
 
-//LiquidCrystal lcd(A5, A4, A3, A2, A1, A0);
+LiquidCrystal lcd(A5, A4, A3, A2, A1, A0);
 volatile unsigned long buffer;
 
 boolean zastaveno;
@@ -32,7 +36,7 @@ void start () {
     casomira = buffer;
     zastaveno = false;
   }
- resetFlag = false;
+  resetFlag = false;
 }
 
 void stop () {
@@ -42,31 +46,60 @@ void stop () {
 }
 
 void vynulovat () {
-  lcd.setCursor(4, 1);
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print("CAS:");
+  lcd.setCursor(8, 1);
   lcd.print("00:00:00");
   radio.write("000000:",7);
   zastaveno = true;
   resetFlag = true;
+  if ((digitalRead(3)) == LOW) {
+       lcd.clear();
+lcd.setCursor(0, 0);
+ lcd.print("stisknuto STOP!!");
+ lcd.setCursor(0, 1);
+ lcd.print("! zkontroluj !!");}
+ else 
+  
+  {
+   
+   lcd.setCursor(0, 0);
+  lcd.print("   TFA Stopky   ");
+}
 }
 
 void setup() {
   radio.begin();
  // Serial.begin(9600);
-  //radio.setChannel(0);
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
+ if (digitalRead(7) == LOW){
+  radio.setChannel (125) ;}
+ else
+ {radio.setChannel (0);}
+// radio.setChannel(0);
+  radio.setPALevel (RF24_PA_MAX);
+ radio.setDataRate( RF24_250KBPS );
+   // radio.setRetries(3,5); // delay, count
+    
   radio.stopListening();
+  radio.openWritingPipe(address);
   pinMode(2, INPUT_PULLUP);
   pinMode(3, INPUT_PULLUP);
   pinMode(4, INPUT_PULLUP);
   pinMode(5, INPUT_PULLUP);
   pinMode(6, INPUT_PULLUP);
+   pinMode(7, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(2), start, FALLING);
   attachInterrupt(digitalPinToInterrupt(3), stop, FALLING);
   lcd.begin(20, 4);
   lcd.setCursor(0, 0);
-  lcd.print("TFA Stopky- r0.8");
-  delay(1000);
+  lcd.print("TFA Stopky - RF");
+  lcd.setCursor(0, 1);
+  lcd.print("verze 2019.08.29");
+  delay(2500);
+  lcd.clear();
+  lcd.setCursor(3, 0);
+  lcd.print("TFA Stopky");
 }
 
 void loop() {
@@ -91,7 +124,9 @@ void loop() {
        sprintf(vystupRF, "%02u%02u%02u:", minuty, sekundy, milisekundy);
     // Serial.println(vystupRF);
       radio.write(vystupRF,7);
-      lcd.setCursor(4, 1);
+            lcd.setCursor(0, 1);
+  lcd.print("CAS:");
+      lcd.setCursor(8, 1);
       lcd.print(vystup);
       displayRefreshTime = 0;
     }
@@ -103,14 +138,16 @@ void loop() {
       char vystup[8];
       char vystupRF[7];
       zobrazStop = false;
-      uint8_t minuty = buffer / 60000;
+           uint8_t minuty = buffer / 60000;
       uint8_t sekundy = (buffer % 60000) / 1000;
       uint8_t milisekundy = buffer % 1000 / 10;
        //sprintf(vystupRF, "%02u%02u%02u:", minuty, sekundy, milisekundy);
       sprintf(vystup, "%02u:%02u%:02u", minuty, sekundy, milisekundy);
-      sprintf(vystupRF, "%02u%02u%02u:", minuty, sekundy, milisekundy);
+  sprintf(vystupRF, "%02u%02u%02u:", minuty, sekundy, milisekundy);
       radio.write(vystupRF, 7);
-      lcd.setCursor(4, 1);
+      lcd.setCursor(0, 1);
+  lcd.print("CAS:");
+      lcd.setCursor(8, 1);
       lcd.print(vystup);
     }
     if(zastaveno && stopRefresh > 1000){
@@ -125,4 +162,5 @@ void loop() {
       radio.write(vystupRF, 7);
       stopRefresh = 0;
     }
+     
 }
