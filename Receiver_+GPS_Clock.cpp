6d4aaf6b7dfda103392af8626b50215fa363fd79
rakat0 <52248245+rakat0@users.noreply.github.com>
2019-09-02 07:42:16 +0200

    Update Receiver_+GPS_Clock.cpp

--- Receiver_+GPS_Clock.cpp
+++ Receiver_+GPS_Clock.cpp
@@ -1,26 +1,41 @@
 // TFA STOPKY  displej
-// verze 1.0 30.6.2019
+// verze 1.0  1.9.2019 Beta
 //RF24 prijimac, MAX7219displej a GPS  NEO 6M hodiny
+//adaptivni rizeni jasu
+
+
+
+
+//#include <nRF24L01.h>
+////#include <printf.h>
+//#include <RF24.h>
+#include <RF24_config.h>
 
 
 #include <AltSoftSerial.h>
 #include <Arduino.h>
 #include <RF24.h>
+#include <nRF24L01.h>
 #include "LedControl.h"
 #include <TinyGPS++.h>
 #include "elapsedMillis.h"
+#include <BH1750.h>
 
-RF24 radio(6, 7); // CE, CSN
+BH1750 lightMeter;
+
+
+
+RF24 radio(2, 3); // CE, CSN
 const byte address[8] = "00001";
 
 //
 /***** These pin numbers will probably not work with your hardware *****
- pin 5 is connected to the DataIn 
- pin A5 is connected to the CLK 
- pin A4 is connected to LOAD 
+ pin 4 is connected to the DataIn 
+ pin 6 is connected to the CLK 
+ pin 5 is connected to LOAD 
  We have only a single MAX72XX.
  */
-LedControl lc=LedControl(5,A5,A4,1);
+LedControl lc=LedControl(4,6,5,1);
 
 
 elapsedMillis lastRadio;
@@ -28,55 +43,119 @@ char sSegment[8];
 byte utc1[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,0};
 byte utc2[]={2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,0,1};
 byte Second, Minute, Hour;
+byte light = 15;
+uint16_t lux ;
 bool dot;
+// char txt[6]  ;
+
 AltSoftSerial altSerial;
 TinyGPSPlus gps;
-float delaytime = (200);
+float delaytime = (500);
+unsigned long startMillis;
+unsigned long currentMillis;
+const unsigned long period = 1000;
+
 
 void setup() {
+
+  pinMode(A0,INPUT); // letni /zimni cas
+pinMode(A1,INPUT);   //adaptivni/max jas
+pinMode(A2,INPUT);   // kanal 0/125
+pinMode(A3,INPUT);  // luxmetr mode - funkcni castecne
+
   Serial.begin(9600);
   altSerial.begin(9600);
+  lightMeter.begin();
+  Wire.begin();
+
   lc.shutdown(0,false);
   /* Set the brightness to a medium values */
-  lc.setIntensity(0,2  );
+  lc.setIntensity(0,15);
   /* and clear the display */
   lc.clearDisplay(0);
-  //lc.setScanLimit(0,6);
+  lc.setScanLimit(0,6);
 
-   lc.setChar(0,5,'-',false);
+   lc.setChar(0,5,'8',true);
   delay(delaytime);
-  lc.setRow(0,4,B01110111);
+  lc.setChar(0,4,'8',true);
+  //lc.setRow(0,4,B00110111);
   delay(delaytime);
-  lc.setRow(0,3,B00110111);
+  lc.setChar(0,3,'8',true);
+  //lc.setRow(0,3,B00110111);
   delay(delaytime);
-  lc.setRow(0,2,B01111110);
+  lc.setChar(0,2,'8',true);
+  //lc.setRow(0,2,B01111110);
   delay(delaytime);
-  lc.setRow(0,1,B00111000);
+  lc.setChar(0,1,'8',true);
+  //lc.setRow(0,1,B00111000);
   delay(delaytime);
+  //lc.setChar(0,4,'8',false);
   //lc.setRow(0,1,0x05);
   //delay(delaytime);
-  lc.setChar(0,0,'-',false);
+  lc.setChar(0,0,'8',true);
   delay(2500);
  lc.clearDisplay(0);
 
   radio.begin();
-  //radio.setChannel (0);
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MIN);
+   radio.openReadingPipe(0, address);
+   if (digitalRead(A2) == LOW){
+   radio.setChannel (125) ;}
+   else
+ {radio.setChannel (0);}
+ radio.setDataRate( RF24_250KBPS );
+ radio.setPALevel(RF24_PA_MAX);
+  
   radio.startListening();
+   startMillis = millis();  //initial start time
 }
+
+
 void loop() {
+  if (digitalRead(A1) == LOW){
+currentMillis = millis();  //get the current "time" (actually the number of milliseconds since the program started)
+  if (currentMillis - startMillis >= period)  //test whether the period has elapsed
+  {
+    lux = lightMeter.readLightLevel();
+        light = map(lux, 0, 40000, 0, 15);
+       
+               lc.setIntensity(0,light);
+    startMillis = currentMillis;  //IMPORTANT to save the start time of the current LED state.
+  }
+  }
+ else 
+  {lc.setIntensity(0,15);}
+
+  
+          if (digitalRead(A3) == LOW){
+  char txt[6];
+    // sprintf(txt,lux);
+sprintf(txt,"%04u",lux);
+    for (int i=0, j = 5; i<6; i++, j--)
+    {
+      if(txt[i]!=sSegment[i]){
+    sSegment[i]=txt[i];
+        
+       lc.setChar(0, j, sSegment[i], false);
+          }
+          }
+
+   }
+   else
+   {
+   
+  
   if (radio.available()) {
     char text[7];
     radio.read(text, 7);
-    Serial.print(text);
+   
    if (text[6]==58) {
     lastRadio=0;
-    Serial.print(text);
+  
     for (int i=0, j = 5; i<6; i++, j--)
     {
       if(text[i]!=sSegment[i]){
         sSegment[i]=text[i];
+      
         lc.setChar(0, j, sSegment[i], true);
             }
           }
@@ -84,6 +163,7 @@ void loop() {
       }
 
   while (altSerial.available()){
+   
     if (gps.encode(altSerial.read()))
     {
       if (gps.time.isValid())
@@ -92,19 +172,25 @@ void loop() {
         Minute = gps.time.minute();
         Second = gps.time.second();
         Hour   = gps.time.hour();
-        sprintf(text, "%02u%02u%02u", utc2[Hour], Minute, Second);
-
+        
+        if (digitalRead(A0) == LOW){
+        sprintf(text, "%02u%02u%02u", utc2[Hour], Minute, Second);}
+        else
+        {sprintf(text, "%02u%02u%02u", utc1[Hour], Minute, Second);}
+      
         if (lastRadio>10000 && memcmp(sSegment, text, 7)){
            
           for (int i=0, j = 5; i<6; i++, j--)
           {
               sSegment[i]=text[i];
            dot= Second % 2 ;
-         
-              lc.setChar(0, j, sSegment[i], dot);
+          
+        
+              lc.setChar(0, j, sSegment[i],  dot);
           }
         }
       }
     }
   }
-}
+   }
+  }
